bd3f920bf788b08c74d1c8587757b003d812e9c8
sun <sunminjun89@gmail.com>
2024-03-23 15:24:41 +0900

    3/23

--- 0x12_수학/11_베르트랑공준.cpp
+++ 0x12_수학/11_베르트랑공준.cpp
@@ -1,11 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define SIZE 250000
+
+bool prime[SIZE];
+int count(int, int);
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    fill(prime, prime + SIZE, 1);
+    for(int i = 2; i < SIZE; i++) {
+        if(!prime[i]) continue;
+        for(int j = 2*i; j < SIZE; j += i)
+            prime[j] = 0;
+    }
+
+    while(1) {
+        int n; cin >> n;
+        if(n == 0) return 0;
+        cout << count(n, 2*n) << "\n";
+    }
+}
+int count(int s, int e) {
+    int cnt = 0;
+    for(int i = s+1; i <= e; i++)
+        if(prime[i]) cnt++;
+    return cnt;
 }
 
 // https://www.acmicpc.net/problem/4948
--- 0x12_수학/12_팩토리얼0개수.cpp
+++ 0x12_수학/12_팩토리얼0개수.cpp
@@ -5,7 +5,13 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    int n; cin >> n;
+    int ans = 0;
+    while(n) {
+        ans += n / 5;
+        n /= 5;
+    }
+    cout << ans;
 }
 
 // https://www.acmicpc.net/problem/1676
